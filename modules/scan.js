e97bec096dc41915af391b9b56e5c6904c0eb2d0
Azures <gilleslazure04@gmail.com>
2023-12-21 00:12:29 +0100

    commit

--- modules/scan.js
+++ modules/scan.js
@@ -1,8 +1,9 @@
+const fs = require("fs/promises")
 const path = require("path")
 const fetch = require("node-fetch")
 
 module.exports = {
-    searchAndGetMediaByName: async (name, root) => {
+    searchAndGetMediaByName: async (name, root, mediaDir) => {
         const config = require(path.join(root, "config.json"))
         const apiKey = config.apis.tmdb.token
         const url = `https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent(name.split(" ").join("+"))}&include_adult=true&language=fr-FR&page=1`;
@@ -16,7 +17,7 @@ module.exports = {
         try {
             const response = await fetch(url, options)
             const json = await response.json()
-            console.log(json)
+            await fs.writeFile(path.join(mediaDir, "cache.json"), JSON.stringify(json, null, 4))
         } catch (error) {
             console.log(error)
         }
